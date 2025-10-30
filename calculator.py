f4f156750688e7a812683b7db612529874b12912
ladno <dadadada123n@gmail.com>
2025-10-30 21:27:09 +0500

    Добавлена валидация ввода пользователя

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
+def get_number(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Ошибка: введите число")
+
 # Простой калькулятор
 def add(a, b):
     return a + b
